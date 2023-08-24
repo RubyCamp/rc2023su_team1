2da0d784dbf09cbadbc3accaad0ecf5bf5520632
haruna-r8 <xqmp71180@mineo.jp>
2023-08-24 15:04:11 +0900

    playerが細かく動きます

--- player.rb
+++ player.rb
@@ -6,27 +6,25 @@ class Player < Character
     px = 1
     py = 1
 
-    # distance = brick.get_sensor(DISTANCE_SENSOR, 0)
-    color = brick.get_sensor(COLOR_SENSOR, 2)
+    # distance = @brick.get_sensor(DISTANCE_SENSOR, 0)
+    color = @ev3_controller.get_color
 
-    while true
-      case color
-        when 0 then # とりあえず止まってもらう
-          puts "色がありません"
-          @ev3_controller.stop(1.0)
-        when 1 then # 少し右に曲がる
-          puts "黒色"
-          @ev3_controller.move_zigzag(0.1)
-        when 2 then # 止まってちょっと下がる？
-          puts "青色" 
-          @ev3_controller.move_backward(0.1)
-        when 5 then # ゴールなのでストップ
-          puts "赤色"
-          @ev3_controller.stop(1.0)
-        when 6 then # 前に進む
-          puts "白色"
-          @ev3_controller.move_forward(0.3)
-      end
+    case color
+      when 0.0 then # とりあえず止まってもらう
+        puts "色がありません"
+        @ev3_controller.stop(1.0)
+      when 1.0 then # 少し右に曲がる
+        puts "黒色"
+        @ev3_controller.move_zigzag(0.15)
+      when 2.0 then # 止まってちょっと下がる？
+        puts "青色" 
+        @ev3_controller.move_backward(0.1)
+      when 5.0 then # ゴールなのでストップ
+        puts "赤色"
+        @ev3_controller.stop(0.2)
+      when 6.0 then # 前に進む
+        puts "白色"
+        @ev3_controller.move_forward(0.1)
     end
     # ここまで
 
